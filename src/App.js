7e38d6e5e35f01c31c45b652a3e10584986c93f5
Thanh Tùng Võ <vothanhtung98@gmail.com>
2022-03-25 23:10:15 +0700

    Design Trello Front-End.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,197 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <h1>Test React App</h1>
+    <div className="trello-clone-container">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://files.fullstack.edu.vn/f8-prod/courses/2.png" alt="alt-img" />
+              Title: Try to clone Trello App.
+            </li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://files.fullstack.edu.vn/f8-prod/courses/2.png" alt="alt-img" />
+              Title: Try to clone Trello App.
+            </li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://files.fullstack.edu.vn/f8-prod/courses/2.png" alt="alt-img" />
+              Title: Try to clone Trello App.
+            </li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://files.fullstack.edu.vn/f8-prod/courses/2.png" alt="alt-img" />
+              Title: Try to clone Trello App.
+            </li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://files.fullstack.edu.vn/f8-prod/courses/2.png" alt="alt-img" />
+              Title: Try to clone Trello App.
+            </li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://files.fullstack.edu.vn/f8-prod/courses/2.png" alt="alt-img" />
+              Title: Try to clone Trello App.
+            </li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://files.fullstack.edu.vn/f8-prod/courses/2.png" alt="alt-img" />
+              Title: Try to clone Trello App.
+            </li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://files.fullstack.edu.vn/f8-prod/courses/2.png" alt="alt-img" />
+              Title: Try to clone Trello App.
+            </li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+            <li>Let's try something!</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
+    </div>
   );
 }
 
