b8943798115607adff6566c72b2d0bc0a4f2838e
Thanh Tùng Võ <vothanhtung98@gmail.com>
2022-03-26 22:41:12 +0700

    Clean React Architecture.

---
+++ src/components/Task/Task.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Task.scss';
+
+function Task() {
+    return (
+        <li className="task-item">
+            <img src="https://files.fullstack.edu.vn/f8-prod/courses/2.png" alt="alt-img" />
+            Title: Try to clone Trello App.
+        </li>
+    )
+}
+
+export default Task
