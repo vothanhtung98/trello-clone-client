7ab419dea8a67314b86dd392b0dd5171f29bbb71
Thanh Tùng Võ <vothanhtung98@gmail.com>
2022-12-25 18:38:49 +0700

    Change BE Host to Render

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,5 +1,6 @@
 // export const API_ROOT = 'http://localhost:3333'
-export const API_ROOT = 'https://trello-clone-apis.herokuapp.com'
+// export const API_ROOT = 'https://trello-clone-apis.herokuapp.com'
+export const API_ROOT = 'https://trello-clone-vothanhtung.onrender.com'
 
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM'
